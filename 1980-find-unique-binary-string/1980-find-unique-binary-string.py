a883f09015bf308d346bf254c211b7331f71f6d4
ashnaadil32-ship-it <ashnaadil32@gmail.com>
2026-08-01 17:11:54 +0530

    Time: 0 ms (100%), Space: 12.5 MB (27.12%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,14 +1,15 @@
-class Solution:
-    def findDifferentBinaryString(self, nums: list[str]) -> str:
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: str
+        """
         ans = []
-        
-        # Iterate through each string's index
+
         for i in range(len(nums)):
-            # Look at the character at the diagonal position: nums[i][i]
-            # Flip '0' to '1' and '1' to '0'
             if nums[i][i] == '0':
                 ans.append('1')
             else:
                 ans.append('0')
-                
+
         return "".join(ans)

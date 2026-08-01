fa653f4c4620fef5d7486bf714f7c63e018dc6bf
ashnaadil32-ship-it <ashnaadil32@gmail.com>
2026-08-01 17:16:32 +0530

    Time: 134 ms (51.89%), Space: 55.4 MB (50%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        pos = defaultdict(list)
+
+        for i, num in enumerate(nums):
+            pos[num].append(i)
+
+        ans = [0] * len(nums)
+
+        for indices in pos.values():
+            m = len(indices)
+
+            prefix = [0] * (m + 1)
+            for i in range(m):
+                prefix[i + 1] = prefix[i] + indices[i]
+
+            for i in range(m):
+                idx = indices[i]
+
+                left = idx * i - prefix[i]
+                right = (prefix[m] - prefix[i + 1]) - idx * (m - i - 1)
+
+                ans[idx] = left + right
+
+        return ans

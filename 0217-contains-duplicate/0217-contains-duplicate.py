ede6dd4466f7aabb03f3d45087600d673125d5a1
ashnaadil32-ship-it <ashnaadil32@gmail.com>
2026-08-22 14:33:13 +0530

    Time: 59 ms (5.23%), Space: 37.1 MB (16.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,25 +1,15 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        freq = {}
+    def containsDuplicate(self,nums:list[int]) -> bool:
+        freq =  {}
 
         for num in nums:
             if num in freq:
                 freq[num] += 1
             else:
                 freq[num] = 1
+
         for value in freq.values():
             if value > 1:
-               return True
+                return True
 
-        return False         
-
-# Time Complexity  → O(n)
-# Space Complexity → O(n)            
-# ⭐ Important lesson
-
-# return False ko loop ke bahar rakho.
-
-# Because:
-
-# True milte hi immediately return kar sakte ho, 
-# but False tabhi return karna hai jab poori list check ho chuki ho.
+        return False                     

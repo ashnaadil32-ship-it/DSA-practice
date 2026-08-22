7641eb652e90f930bb883e20e263a73e5653746e
ashnaadil32-ship-it <ashnaadil32@gmail.com>
2026-08-22 15:52:11 +0530

    Time: 61 ms (28.5%), Space: 28.5 MB (75.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,11 +4,8 @@ class Solution:
         best_buy = prices[0]
 
         for i in range(1,len(prices)):
-            if prices[i] <= best_buy:
-                best_buy = min(best_buy,prices[i])
-
-            if prices[i] > maxprofit:
-                maxprofit = max(maxprofit,prices[i] - best_buy)
+            best_buy = min(best_buy,prices[i])
+            maxprofit = max(maxprofit,prices[i] - best_buy)
 
         return maxprofit
       

77d4bfe29ea079d16a3a8c25e93a9b055b5cb50c
Ashna Adil <ashnaadil32@gmail.com>
2026-09-20 16:56:13 +0530

    Time: 6 ms (88.98%), Space: 19.3 MB (74.21%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        ans = []
+
+        for word in words:
+            total = 0
+
+            for ch in word:
+                total += weights[ord(ch) - ord('a')]
+
+            total %= 26
+            ans.append(chr(ord('z') - total))
+
+        return ''.join(ans)

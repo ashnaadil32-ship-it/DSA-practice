fdb88fb792dc4f54221c7246f6cc3eb42ed05c27
Ashna Adil <ashnaadil32@gmail.com>
2026-09-23 11:22:03 +0530

    Time: 84 ms (58.58%), Space: 30.7 MB (81.29%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minOperations(self, nums, x):
+        target = sum(nums) - x
+        
+        if target < 0:
+            return -1
+        
+        left = 0
+        curr = 0
+        max_len = -1
+        
+        for right in range(len(nums)):
+            curr += nums[right]
+            
+            while curr > target and left <= right:
+                curr -= nums[left]
+                left += 1
+            
+            if curr == target:
+                max_len = max(max_len, right - left + 1)
+        
+        if max_len == -1:
+            return -1
+        
+        return len(nums) - max_len
